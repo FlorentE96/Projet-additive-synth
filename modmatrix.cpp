f48c09409b936eb652fe416e516b81feac683de3
FlorentE96 <flo.ecochard@gmail.com>
2017-04-07 18:03:56 +0200

    tests on modmatrix

--- modmatrix.cpp
+++ modmatrix.cpp
@@ -2,5 +2,19 @@
 
 modMatrix::modMatrix()
 {
-
+    reset();
 }
+
+modMatrix::defineSrc()
+
+void connect(Source _src, Dest _dst) {
+    links[_src][_dst].state = true;
+}
+
+void disconnect(Source _src, Dest _dst) {
+    links[_src][_dst].state = false;
+}
+
+template<typename T1>
+float modMatrix::process(T1 * _dst);
+void modMatrix::reset();
--- modmatrix.hpp
+++ modmatrix.hpp
@@ -5,27 +5,32 @@
 class modMatrix
 {
 public:
+//    template<typename T1, typename T2>
+//    struct Link {
+//        T1 * src;
+//        T2 * dst;
+//        Link(T1 * _src, T2 * _dst) {
+//            src = _src;
+//            dst = _dst;
+//        }
+//    };
     template<typename T1, typename T2>
     struct Link {
         T1 * src;
         T2 * dst;
-        Link(T1 * _src, T2 * _dst) {
-            src = _src;
-            dst = _dst;
-        }
+        bool state;
     };
-//    enum Source { ADSR1, ADSR2, LFO1, LFO2, VEL };
-//    enum Dest { oscFreq, oscAmp, filtFreq, filtQ };
+    enum Source { ADSR1=0, ADSR2, LFO1, LFO2, VEL, SRC_SIZE};
+    enum Dest { oscFreq=0, oscAmp, filtFreq, filtQ, DST_SIZE};
     modMatrix();
-    template<typename T1, typename T2>
-    void connect(T1 * _src, T2 * _dst);
-    template<typename T1, typename T2>
-    void disconnect(T1 * _src, T2 * _dst);
+    void connect(Source _src, Dest _dst);
+    void disconnect(Source _src, Dest _dst);
     template<typename T1>
     float process(T1 * _dst);
     void reset();
 private:
-    std::vector<Link> links;
+    //std::vector<Link> links;
+    Link links[SRC_SIZE][DST_SIZE];
 };
 
 #endif // MODMATRIX_HPP
