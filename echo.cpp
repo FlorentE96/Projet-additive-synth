c95eedf8b784c714b08a0168d14b76e6aa08a43e
MathieuGuillaume0 <mguillaume00@gmail.com>
2017-04-14 17:06:53 +0200

    finished first implementation of echo effect, not working

--- echo.cpp
+++ echo.cpp
@@ -1,49 +1,63 @@
 #include "effects.hpp"
 
-
-uint16_t Echo::getD(){
+uint32_t Echo::getD(){
 	return D;
 }
-
-void Echo:setD(uint16_t _D){
-	D = _D;
+float Echo::getFb(){
+    return Fb;
 }
 
-Echo:Echo(uint16_t _D, float _G, float _DryWet){
+void Echo::setD(uint32_t _D){
+    if(_D > MAX_DELAY){
+        D = MAX_DELAY;
+    }
+    else{
+        D = _D;
+    }
+
+}
+
+void Echo::setFb(float _Fb){
+    Fb = _Fb;
+}
+
+Echo::Echo(uint32_t _D, float _Fb, float _DryWet){
 	setD(_D);
+    setFb(_Fb);
+    delayed_sample = new int16_t[3*D];
 	
 }
 
-Echo:~Echo(){
+Echo::~Echo(){
+    delete [] delayed_sample;
 		
 }
 
-int16_t Echo::delayCompute (int16_t input){
+int16_t Echo::echoCompute (int16_t input){
 	
-	static int i = 0;
-	static int16_t delayed_sample[MAX_DELAY];
-	
-	shiftSamples(delayed_sample, input);
+    static unsigned int i = 0;
 
-	if (i < Echo.D){
+	
+    shiftSamples(input);
+
+    if (i < D){
 		i++;
 		return input;
 	}
 	else{
-		return input + (int16_t)(delayed_sample[0]);
+        return input + (int16_t) (Fb*delayed_sample[2*D]+ pow((double)Fb,2.0f)*delayed_sample[D] + pow((double)Fb,3.0f)*delayed_sample[0] )  ;
 	}
 }
 
 
-void Echo::shiftSamples (int16_t delayed_sample[], int16_t input){
+void Echo::shiftSamples (int16_t input){
 	
-	for(int i = 0; i< Echo.D; i++){
-		if (i == (Echo.D - 1) ) {
+    for(uint32_t i = 0; i< 3*D; i++){
+        if (i == (3*D - 1) ) {
 			delayed_sample[i] = input;
 		}
 		else{
-		delayed_sample[i] = delayed_sample[i-1];
+            delayed_sample[i] = delayed_sample[i+1];
 		}
 	}
-	
 }
--- effects.hpp
+++ effects.hpp
@@ -1,20 +1,24 @@
-#define MAX_DELAY 44100
-
-
+#include <cstdint>
+#include <cmath>
+#include "global.hpp"
+#define MAX_DELAY (220500) // 5sec
 
 class Echo{
 private:
-	
-	uint16_t D;
-	float G;
-	float DryWet;
-	void shiftSamples (int16_t delayed_sample[], int16_t input);
-	
+    uint32_t D;
+    float Fb;
+    float DryWet;
+    void shiftSamples ( int16_t input);
+    int16_t* delayed_sample;
 public:
-	uint16_t getD();
-	void setD(uint16_t _D);
-	int16_t echoCompute (int16_t input);
-	Echo(uint16_t _D, float _G, float _DryWet);	
-	~Echo();
-	
+    uint32_t getD();
+    float getFb();
+    void setD(uint32_t _D);
+    void setFb(float _Fb);
+
+    Echo(uint32_t _D, float _Fb, float _DryWet);
+    ~Echo();
+    int16_t echoCompute (int16_t input);
+
+
 };
