c1cec28af300cb88098ebe0368b6d4a616c5e6aa
MathieuGuillaume0 <mguillaume00@gmail.com>
2017-04-16 14:51:14 +0200

    Added DryWet and ON/OFF status to echo effect

--- echo.cpp
+++ echo.cpp
@@ -35,23 +35,40 @@ float Echo::getTd(){
 }
 
 void Echo::setDryWet(float _DryWet){
-    DryWet = _DryWet;
+    if (_DryWet < 0) {DryWet = 0;}
+    else if (_DryWet > 1) {DryWet = 1;}
+    else {DryWet = _DryWet;}
+
 }
 float Echo::getDryWet(){
     return DryWet;
+}
+
+void Echo::setStatus(uint32_t _status){
+  if (_status > 0){
+    status = ON;
+  }
+  else{
+    status = OFF;
+  }
+}
+uint32_t Echo::getStatus(){
+  return status;
 }
 
 Echo::Echo(float _Td, float _Fb, float _DryWet){
 		setTd(_Td);
     setFb(_Fb);
 		setDryWet(_DryWet);
-    delayed_sample = new int16_t[3*D];
+    setStatus(OFF);
+    delayed_sample = new int16_t[3*D+1];
 
 }
 Echo::Echo(uint32_t _D, float _Fb, float _DryWet){
 		setD(_D);
     setFb(_Fb);
 		setDryWet(_DryWet);
+    setStatus(OFF);
     delayed_sample = new int16_t[3*D+1];
 
 }
@@ -59,22 +76,43 @@ Echo::~Echo(){
     delete [] delayed_sample;
 
 }
+int16_t Echo::echoEffect (int16_t input){
+  if(status == ON){
+    return ( (int16_t)(DryWet*input +(1-DryWet)*echoCompute(input)) );
+  }
+  else{
+    return input;
+  }
+}
 
 int16_t Echo::echoCompute (int16_t input){
 
     static unsigned int i = 0;
-
     shiftSamples(input);
 
     if (i < D){
-			i++;
+      i++;
 			return 0;
+
 		}
 		else {
 			return (int16_t) (Fb*delayed_sample[2*D]+ pow((double)Fb,2.0f)*delayed_sample[D] + pow((double)Fb,3.0f)*delayed_sample[0] )  ;
 		}
 	}
 
+int16_t Echo::echoComputev2 (int16_t input){
+
+    static unsigned int i = 0;
+    storeSamples(input, i%(3*D+1));
+    i++;
+
+    if (i < D){
+			return 0;
+		}
+		else {
+			return (int16_t) (Fb*delayed_sample[(2*D+i)%(3*D+1)]+ pow((double)Fb,2.0f)*delayed_sample[(D+i)%(3*D+1)] + pow((double)Fb,3.0f)*delayed_sample[i%(3*D+1)] )  ;
+		}
+	}
 
 
 void Echo::shiftSamples (int16_t input){
@@ -87,6 +125,10 @@ void Echo::shiftSamples (int16_t input){
         	delayed_sample[i] = delayed_sample[i+1];
 				}
 	}
+}
+
+void Echo::storeSamples(int16_t input, uint32_t i){
+    delayed_sample[3*D-i] = input;
 }
 
 
@@ -110,13 +152,16 @@ bool saveArrayToFile(const char* filename, int16_t* array, uint32_t lenArray){
 
 int main(int argc, char* argv[]){
 
-	Echo* myEcho = new Echo((uint32_t)40, 0.5f, 0.1f);
+	Echo* myEcho = new Echo((uint32_t)40, 1.5f, 0.1f);
 	myEcho->setFb(0.5f);
+  myEcho->setDryWet(0.9f);
+  myEcho->setStatus(ON);
 
 	int lenArray = 125;
 
 	int16_t inputArray[lenArray];
 	int16_t outputArray[lenArray];
+  double sum = 0;
 
 	for (int i = 0; i < lenArray; i++) {
 		if (  i < 1 ){
@@ -125,9 +170,14 @@ int main(int argc, char* argv[]){
 		else{
 			inputArray[i] = 0;
 		}
-
-		outputArray[i] = myEcho->echoCompute(inputArray[i]);
+    clock_t begin = clock();
+		outputArray[i] = myEcho->echoEffect(inputArray[i]);
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    sum += elapsed_secs;
 	}
+  double avg_elapsed_secs = sum/lenArray;
+  cout << "Average execution time of echoEffect function : " << avg_elapsed_secs << " s." << endl;
 
 	saveArrayToFile(argv[1], inputArray, lenArray);
 	saveArrayToFile(argv[2], outputArray, lenArray);
--- effects.hpp
+++ effects.hpp
@@ -2,20 +2,25 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <ctime>
 #include "global.hpp"
 #define MAX_DELAY (220500) // 5sec
 
 using namespace std;
 class Echo{
 private:
+    uint32_t status;
     float Td; //in s
     uint32_t D;
     float Fb;
     float DryWet;
     int16_t* delayed_sample;
     void shiftSamples ( int16_t input);
+    void storeSamples(int16_t input, uint32_t i);
 
 public:
+    void setStatus(uint32_t _status); // set status effect ON before using it
+    uint32_t getStatus();
     void setTd(float _Td);
     float getTd();
     void setD(uint32_t _D);
@@ -28,7 +33,9 @@ public:
     Echo(float _Td, float _Fb, float _DryWet);
     Echo(uint32_t D, float _Fb, float _DryWet);
     ~Echo();
-    int16_t echoCompute (int16_t input);
+    int16_t echoCompute (int16_t input); // return echo samples
+    int16_t echoComputev2 (int16_t input); // return echo samples -- optimized  (not working properly)
+    int16_t echoEffect (int16_t input); // return echo samples + input with DryWet mixing factor
 
 
 };
